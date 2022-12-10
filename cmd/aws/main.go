5d3c4357190f1b4ca7aea6ddb02ba73261795990
Josemy Duarte <duartejosemy@gmail.com>
2022-12-10 16:16:01 +0000

    DRY assets paths

--- cmd/aws/main.go
+++ cmd/aws/main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"image/png"
-	"log"
-	"net/http"
-	"strconv"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 
-	"github.com/josemyduarte/printer/internal/printer"
+	"github.com/josemyduarte/printer/internal/handler"
 )
 
 var (
@@ -36,65 +28,13 @@ func init() {
 	}
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Printf("request received [%+v]", request)
-
-	var ParsedRequest struct {
-		Text string `json:"text"`
-	}
-
-	b := []byte(request.Body)
-	err := json.Unmarshal(b, &ParsedRequest)
-	if err != nil {
-		log.Printf("failed to parse request body: %w", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusBadRequest,
-			Body:       err.Error(),
-		}, err
-	}
-
-	log.Printf("text = [%+v]", ParsedRequest.Text)
-
-	img, err := printer.TextOnImg(printer.Request{
-		BgImgPath: bgFileName,
-		FontPath:  fontFileName,
-		FontSize:  60,
-		Text:      ParsedRequest.Text,
-	})
-	if err != nil {
-		log.Printf("failed to print text on image: %w", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
-		}, err
-	}
-
-	log.Printf("printed %s on image", ParsedRequest.Text)
-
-	imgBuf := new(bytes.Buffer)
-	if png.Encode(imgBuf, img) != nil {
-		log.Printf("failed to encode image bytes: %w", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
-		}, err
-	}
-
-	log.Printf("image encoded")
-
-	return events.APIGatewayProxyResponse{
-		StatusCode:      http.StatusOK,
-		Body:            base64.StdEncoding.EncodeToString(imgBuf.Bytes()),
-		IsBase64Encoded: true,
-		Headers: map[string]string{
-			"Content-Type":   "image/png",
-			"Content-Length": strconv.Itoa(len(imgBuf.Bytes())),
-		},
-	}, nil
-
-}
-
 func main() {
+	awsHandler := handler.AWS{
+		BackgroundImgPath: bgFileName,
+		FontPath:          fontFileName,
+		FontSize:          60,
+	}
+
 	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(handler)
+	lambda.Start(awsHandler.Serve)
 }
